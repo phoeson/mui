ccd4c26ce0b3d41bc051c5bf95712e47d8fee752
Phoeson <fengxpan@gmail.com>
2026-05-28 14:34:02 +0800

    Add Card control

---
+++ 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/Content/ControlsStylesContainers.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace FirstFloor.ModernUI.App.Content
+{
+    public partial class ControlsStylesContainers : UserControl
+    {
+        public ControlsStylesContainers()
+        {
+            InitializeComponent();
+        }
+    }
+}
